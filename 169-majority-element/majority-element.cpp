2f5296c5820a5f5f6cc08ceba021359f5df2fca2
Riya Malik <105075030+Riya1922@users.noreply.github.com>
2025-08-25 10:21:46 +0530

    Time: 0 ms (100.00%) | Memory: 28 MB (86.86%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -2,15 +2,25 @@ class Solution {
 public:
 //Riya Malik
     int majorityElement(vector<int>& nums) {
-        unordered_map<int, int> mp;
-        int n =  nums.size();
-        for(auto &it: nums) mp[it]++;
+        //step 1: Apply Moore's Voting Algorithm
+       int n = nums.size();
+       int cnt=0, ele;
+       for(int i=0; i<n; i++){
+           if(cnt==0){
+              cnt = 1;
+              ele = nums[i];
+           }
+           else if(nums[i]==ele) cnt++;
+           else cnt--;
+       }
 
-        for(auto [el, f]: mp){
-            if(f>(n/2)){
-                return el;
-            }
-        }
-        return -1;
+       //step 2: verify if the ele is majority ele or not
+       int cnt2=0;
+       for(int i=0; i<n; i++){
+        if(nums[i]==ele) cnt2++;
+       }
+
+       if(cnt2 > n/2) return ele;
+       else return -1;
     }
 };
